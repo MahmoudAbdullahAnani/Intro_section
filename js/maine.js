6461bc6195c81c1cd21a8a5fd223c58a6485e5a2
MahmoudAbdullahAnani <mahmoud18957321@gmail.com>
2022-05-05 17:23:54 +0200

    creat ptoject

---
+++ js/maine.js
@@ -0,0 +1,63 @@
+let features = document.querySelector('#nav-last-1');
+let img1 = document.querySelector('#img1');
+let mybody = document.querySelector('body');
+let myPopup = document.querySelector('.ss');
+let myNavOne = document.querySelector('#nav-last-1');
+let v = document.querySelector('.v');
+let j = document.querySelector('.j');
+let n = document.querySelector('.n');
+let l = document.querySelector('.l');
+let q = document.querySelector('.q');
+let b = document.querySelector('.z');
+let z = document.querySelector('.b');
+
+features.onclick = function () {
+    img1.src = "./images/icon-arrow-up.svg";
+    mybody.style.background = "rgba(162, 162, 162, 0.434)"
+    myPopup.style.display = 'block';
+    myNavOne.style.color = 'red';
+    myNavOne.style.testShadow = '0px 0px 7px #000000'
+    // myPopup.style = '751';
+}
+v.onclick = function () {
+    img1.src = "./images/icon-arrow-down.svg";
+    mybody.style.background = "#ffffff"
+    myPopup.style.display = 'none';
+    myNavOne.style.color = '#000000';
+}
+j.onclick = function () {
+    img1.src = "./images/icon-arrow-down.svg";
+    mybody.style.background = "#ffffff"
+    myPopup.style.display = 'none';
+    myNavOne.style.color = '#000000';
+}
+n.onclick = function () {
+    img1.src = "./images/icon-arrow-down.svg";
+    mybody.style.background = "#ffffff"
+    myPopup.style.display = 'none';
+    myNavOne.style.color = '#000000';
+}
+l.onclick = function () {
+    img1.src = "./images/icon-arrow-down.svg";
+    mybody.style.background = "#ffffff"
+    myPopup.style.display = 'none';
+    myNavOne.style.color = '#000000';
+}
+q.onclick = function () {
+    img1.src = "./images/icon-arrow-down.svg";
+    mybody.style.background = "#ffffff"
+    myPopup.style.display = 'none';
+    myNavOne.style.color = '#000000';
+}
+b.onclick = function () {
+    img1.src = "./images/icon-arrow-down.svg";
+    mybody.style.background = "#ffffff"
+    myPopup.style.display = 'none';
+    myNavOne.style.color = '#000000';
+}
+z.onclick = function () {
+    img1.src = "./images/icon-arrow-down.svg";
+    mybody.style.background = "#ffffff"
+    myPopup.style.display = 'none';
+    myNavOne.style.color = '#000000';
+}
